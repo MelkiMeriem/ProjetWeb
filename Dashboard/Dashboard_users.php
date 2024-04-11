aafa817b16d7410109e9e2bb51bb2e6e2c30705d
TalbiHamza <hamza.talbi@insat.ucar.tn>
2024-04-12 00:53:11 +0100

    comm

--- Dashboard/Dashboard_users.php
+++ Dashboard/Dashboard_users.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Campaigns Dashboard</title>
     <link rel="stylesheet" href="./Dashboard.css">
+    <title>Users_Table</title>
 </head>
 <body>
-    <p1 id="title">Campaigns Dashboard</p1>
+    <p1 id="title">Users_Table</p1>
     <hr>
 
     <?php
     include('./DB_Connexion.php');
     include('./TableDisplay.php');
-        $sql = "SELECT * FROM campaign";
+        $sql = "SELECT * FROM user";
         $result = mysqli_query($conn, $sql);
 
         $data = []; // Initialize empty array for table data
@@ -24,35 +24,33 @@
         }
 
         // Table headers for campaigns
-        $tableHeaders = ["Name", "Purpose", "Owner Email", "Owner Password", "Campaing ID", "Budget", "Imagelink"];
+        $tableHeaders = ["Name", "Age", "Email", "Password"];
 
         // Call displayTable function
         displayTable($data, $tableHeaders, "CampaignID", "Campaign");
 ?>
 
-    <div id="container">
-        <form action="Dashboard_campaigns.php" method="post">
-            <input type="text" name="id" id="input1" required>
-            <div class="labelline">Delete Campaign</div>
+<div id="container">
+        <form action="Dashboard_users.php" method="post">
+            <input type="text" name="email" id="input1" required>
+            <div class="labelline">Delete User</div>
             <br>
             <input type="submit" name="submit" value="Delete" id="submit">
         </form>
 
         <?php
-        require_once('DeleteData.php'); // Include the deleteData.php file
+        require_once('./DeleteData.php'); // Include the deleteData.php file
 
         if (isset($_POST['submit'])) {
-          if (isset($_POST['id']) && !empty($_POST['id'])) {
-            $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
-            deleteData($conn, 'campaign', 'CampaignID', $id); // Call the delete function
+          if (isset($_POST['email']) && !empty($_POST['email'])) {
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+            deleteData($conn, 'user', 'email', $email); // Call the delete function
           } else {
-            echo "<p>Please enter a Campaign ID to delete.</p>";
+            echo "<p>Please enter a user email to delete.</p>";
           }
         }
         mysqli_close($conn);
         ?>
     </div>
-
-
 </body>
 </html>
