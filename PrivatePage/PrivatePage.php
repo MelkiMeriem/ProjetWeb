6a0fd37001b93083e9e54f796bec299c0e53547a
haythem <haythem.krid@insat.ucar.tn>
2024-04-17 20:03:34 +0100

    progression

--- PrivatePage/PrivatePage.php
+++ PrivatePage/PrivatePage.php
@@ -1,8 +1,40 @@
 
+<<<<<<< HEAD
 <?php
 include("../public/menu2.php");
 ?>  
 <div class="w3-main" style="margin-left:340px;margin-right:40px">
+=======
+    <title>Document</title>
+</head>
+<body>
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
+    <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
+    <div class="w3-container">
+      <h3 class="w3-padding-64"><b>Manara</b></h3>
+    </div>
+    <div class="w3-bar-block">
+    <a href="../PrivatePage/PrivatePage.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a> 
+      <a href="../CreateCampaign/CreateCampaign.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Create Campaign</a> 
+      <a href="../Account/Account.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Account</a> 
+      <a href="../HomePage/HomePage.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Log out</a> 
+  
+    </div>
+  </nav>
+  
+  <!-- Top menu on small screens -->
+  <header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+      <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()">☰</a>
+      <span>Manara</span>
+    </header>
+  <!-- Overlay effect when opening sidebar on small screens -->
+  <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+  
+  <!-- !PAGE CONTENT! -->
+  <div class="w3-main" style="margin-left:340px;margin-right:40px">
+>>>>>>> a4f4117 (progression)
   
     <!-- Header -->
     <div class="w3-container" style="margin-top:80px" id="showcase">
